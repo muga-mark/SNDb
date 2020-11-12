505c4b89f76cea66509aa8321ef5f8be0624d77a
muga-mark <markmuga01@gmail.com>
2020-11-12 19:53:10 +0800

    added film  rating, added filter for crews

--- src/pages/InfoMovie.js
+++ src/pages/InfoMovie.js
@@ -41,13 +41,25 @@ function InfoMovie({ }) {
     const [open, setOpen] = useState(false);
     const [ movieDetails, setMovieDetails ] = useState([]);
     const [ movieDetailsLoading, setMovieDetailsLoading ] = useState(true);
+    const [ crews, setCrews ] = useState([]);
+    const [ cast, setCast ] = useState([]);
     const [trailer, setTrailer] = useState([]);
-    const credits = movieDetails.credits;
+
+    const [ contentRating, setContentRating ] = useState([]);
+    
+
+
     const totalMinutes = movieDetails.runtime;
     const hours = Math.floor(totalMinutes / 60);          
     const minutes = totalMinutes % 60;
 
     
+
+   
+
+   
+
+    const GET_MOVIE_CONTENT_RATING = `https://api.themoviedb.org/3/movie/${movieId}/release_dates?api_key=${API_KEY}`;
     const GET_MOVIE_DETAILS = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US&append_to_response=credits`;
     const MOVIE_TRAILER_API = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`;
     const trailer_url = `https://www.youtube.com/watch?v=${trailer}`;
@@ -72,7 +84,7 @@ function InfoMovie({ }) {
         })
 
 
-    }, [ MOVIE_TRAILER_API ])
+    }, [ MOVIE_TRAILER_API ]);
 
     useEffect(() => {
         fetch(GET_MOVIE_DETAILS)
@@ -82,13 +94,61 @@ function InfoMovie({ }) {
             if(data){
                 setMovieDetails(data);
                 setMovieDetailsLoading(false);
+                setCrews(data.credits.crew);
+                setCast(data.credits.cast);
             }
           
         })
 
 
-      }, [ GET_MOVIE_DETAILS ])
+    }, [ GET_MOVIE_DETAILS ]);
+
+    useEffect(() => {
+        
+        fetch(GET_MOVIE_CONTENT_RATING)
+        .then(res => res.json())
+        .then(data => {
+          console.log("rating",data.results);
+            if(data){
+                const contentRatingResult = data.results.filter(function(el){
+                    return  el.iso_3166_1 === "US" || el.iso_3166_1 === "CA";
+                });
+        
+                setContentRating(contentRatingResult[0]?.release_dates[0]);
+            }
+          
+        })
+
+
+    }, [ GET_MOVIE_CONTENT_RATING ])
     
+
+    // const crewNew = credits.crew;
+    // console.log("CREW NEW", crews);
+
+    const crewWriters = crews.filter(function(el){
+        return  el.department === "Writing" &&
+                (el.job === "Screenplay" || el.job === "Writer" || el.job === "Novel");
+    });
+
+    // console.log("Screenplay Array", crewWriters);
+
+    const crewDirector = crews.filter(function(el){
+        return  el.department === "Directing" &&
+                el.job === "Director";
+    });
+
+    // console.log("Director Array", crewDirector);
+
+    // const contentRatingResult = contentRating.filter(function(el){
+    //     return  el.iso_3166_1 === "US";
+    // });
+    // const [ certification, setCertification ] = useState(contentRatingResult);
+    // setContentRating(contentRatingResult[0].release_dates);
+    console.log("contentRatingResult>>>>", contentRating);
+    // console.log("certification", certification);
+
+    // console.log("contentRating",contentRating);
 
     return (
         <div className="page">
@@ -116,9 +176,49 @@ function InfoMovie({ }) {
                                 <span>
                                     {movieDetails.title}
                                 </span>
+                                
+                                <div className="movie__ratings_container">
+                                    <span className="movie__ratings marginRightLeft">
+                                        <StarIcon /> 
+                                    </span>
+
+                                    <div>
+                                        <div>    
+                                            <span className="movie__ratings">
+                                                {movieDetails.vote_average}
+                                            </span>
+                                            <span className="movie__ratings_small">
+                                                /10
+                                            </span>
+                                        </div>   
+
+                                        <div>
+                                            <span className="movie__ratings_small marginRightLeft">
+                                                ({movieDetails.vote_count})
+                                            </span>
+                                        </div> 
+                                    </div>
+                                    
+                            </div>
                             </div>
 
                             <div className="movie__genre_container">
+
+                                {contentRating.certification ?
+                                    <div className="movie__genre">
+                                        <span>{contentRating.certification}</span>
+                                    </div>
+                                :
+                                    <div className="movie__genre">
+                                        <span>No Rating</span>
+                                    </div>
+                                }
+                                
+
+                                <div className="movie__genre marginRightLeft">
+                                    <span>|</span>
+                                </div>
+
                                 {movieDetails.genres
                                     .map((result) => (
                                         <div key={result.id} className="movie__genre">
@@ -152,25 +252,7 @@ function InfoMovie({ }) {
                                 </div>
                             </div>
 
-                            <div className="movie__ratings_container marginTop">
-                                <span className="movie__ratings marginRightLeft">
-                                    <StarIcon /> 
-                                </span>
-                                
-                                <span className="movie__ratings">
-                                    {movieDetails.vote_average}
-                                </span>
-
-                                <div>
-                                    <span className="movie__genre">
-                                        /10
-                                    </span>
-                                    
-                                    <span className="movie__genre marginRightLeft">
-                                        ({movieDetails.vote_count})
-                                    </span>
-                                </div>
-                            </div>
+                            
 
                             <div className="movie__overview_container">
                                 <span className="movie__genre "> 
@@ -179,49 +261,80 @@ function InfoMovie({ }) {
                             </div>
 
                             
-                            {/* <div className="movie__crew">
-                                <div className="director">
-                                    <span className="movie__genre">
-                                        Director: 
+                            <div className="movie__crew">
+                                <div className="director__container marginTop ">
+                                    <span className="movie__genre marginRight width">
+                                        {crewDirector.length>1? " Directors: ": "Director: "}
                                     </span>
-                                    {credits.crew
-                                        .map((result) => (
-                                        <div key={result.crew_id} className="movie__genre">
-                                            
-                                            {result.department === "Directing"? 
+                                    {crewDirector.length>0?
+                                        <div className="director">
+                                            {crewDirector.length>1?
                                                 <>
-                                                    <span className="movie__genre">
-                                                        {result.name}
-                                                    </span>
+                                                    {crewDirector
+                                                        .map((result) => (
+                                                        <div key={result.crew_id}>
+                                                            <span className="movie__genre">
+                                                                {result.name}
+                                                            </span>
+                                                        </div>))
+                                                        .reduce((prev, curr) => [prev, <span className="movie__genre marginRight"> ,</span>, curr])
+                                                    }
                                                 </>
-                                            :null}
-                                        </div>))
-                                    }
+                                            :
+                                                <>
+                                                    {crewDirector
+                                                        .map((result) => (
+                                                        <div key={result.crew_id} >
+                                                            <span className="movie__genre">
+                                                                {result.name}
+                                                            </span>
+                                                        </div>))
+                                                    }
+                                                </>
+                                            }
+                                        </div>
+                                    :null}
                                 </div>
 
-                                <div className="writers">
-                                    <span className="movie__genre">
-                                        Writers: 
-                                    </span>
-                                    {credits.crew
-                                        .map((result) => (
-                                        <div key={result.crew_id}>
-                                            
-                                            {result.department === "Writing"? 
-                                                    <>
-                                                        <span className="movie__genre">
-                                                            {result.name}
-                                                        </span>
+                                {crewWriters.length>0?
+                                    <div className="writers__container marginTop">
+                                        <span className="movie__genre marginRight width">
+                                            {crewWriters.length>1? " Writers:" : "Writer:"}
+                                        </span>
+                                        <div className="writers">
+                                            {crewWriters.length>1?
+                                                <>
+                                                    {crewWriters
+                                                        .map((result) => (
+                                                        <div key={result.crew_id}>
+                                                            <span className="movie__genre ">
+                                                                {result.job === "Writer" ?
+                                                                <>{result.name}</>
+                                                                :<>{result.name} ({result.job})</>}
+                                                            </span>
+                                                        </div>))
+                                                        .reduce((prev, curr) => [prev, <span className="movie__genre marginRight"> ,</span>, curr])
+                                                    }
+                                                </>
+                                            :
+                                                <>
+                                                    {crewWriters
+                                                        .map((result) => (
+                                                        <div key={result.crew_id}>
+                                                            <span className="movie__genre ">
+                                                                {result.job === "Writer" ?
+                                                                <>{result.name}</>
+                                                                :<>{result.name} ({result.job})</>}
+                                                            </span>
+                                                        </div>))
+                                                    }
+                                                </>
+                                            }
+                                        </div>
+                                    </div>
+                                :null}
 
-                                                        <span className="movie__genre">
-                                                            ({result.job})
-                                                        </span>
-                                                    </>
-                                                :null}
-                                        </div>))
-                                    }
-                                </div>
-                            </div> */}
+                            </div>
 
                             
                             
